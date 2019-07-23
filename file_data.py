1056efa0c2e82992ecc33f040c235014da430c5f
anantharaman <anantharaman@hoovi.in>
2019-07-23 13:45:16 +0530

    2 apis done

--- file_data.py
+++ file_data.py
@@ -12,18 +12,22 @@ def index():
   return 'Server Works!'
 
 
-unpacked_brands = json.loads(redis_connector.r.get('brands'))
-pp = [x for x in unpacked_brands if x['colors'] == "blue"]
-out_put=  json.dumps(pp)
-#print(out_put)
+
+
 @app.route('/getRecentItem', methods=['GET','POST'])
 def get_recent_item_by_date():
 	unpacked_brands = json.loads(redis_connector.r.get('brands'))
 	date_added = request.form['date_added']
-	# date_added = '23-07-2019'
 	pp = [x for x in unpacked_brands if x['dateAdded'] == date_added]
-	print(pp)
-	return json.dumps(pp)
+	json_list = json.dumps(pp)
+	return json_list
+
+@app.route('/getItemsbyColor', methods=['GET','POST'])
+def get_items_by_color():
+	unpacked_brands = json.loads(redis_connector.r.get('brands'))
+	color = request.form['color']
+	pp = [x for x in unpacked_brands if x['colors'] != color]
+	rest_list = pp[:10]
+	return json.dumps(rest_list)
 
 
-#unpacked_images = json.loads(r.get('brands').decode('utf-8'))
